9711ebc710aa43657141963f717c31b11ecf0666
Ljourn Llagas <40813696+ljournllagas@users.noreply.github.com>
2019-11-27 16:15:49 +0800

    - Remove the default model and controller - Add Microsoft.AspNetCore.Mvc.NewtonsoftJson for Json Serializer/Deserializer - Add Customer Model

---
+++ IntroToDotNetCoreWebAPI/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroToDotNetCoreWebAPI.Models
+{
+    public class Customer
+    {
+        public Customer()
+        { }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string Citizenship { get; set; }
+        public string EmailAddress { get; set; }
+
+
+    }
+}
